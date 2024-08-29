daf473eb3ebc813a6d598ccb671cf0567eb21330
sava-evangelista <sava.evangelista@gmail.com>
2024-08-28 22:43:26 -0400

    Upload project

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
+// create classes
+const CROSS = 'cross'
+const CIRCLE = 'circle'
+
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -10,32 +12,54 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+
+// create variables for elements
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
+const winMsg = document.getElementById('winningMessage')
 const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const winMsgTxt = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
 startGame()
 
 restartButton.addEventListener('click', startGame)
 
+// clears screen & class
 function startGame() {
   circleTurn = false
   cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
+    cell.classList.remove(CROSS)
+    cell.classList.remove(CIRCLE)
     cell.removeEventListener('click', handleClick)
     cell.addEventListener('click', handleClick, { once: true })
   })
   setBoardHoverClass()
-  winningMessageElement.classList.remove('show')
+  winMsg.classList.remove('show')
+  const p2Icon = document.getElementById('p2-turn-dot')
+  p2Icon.classList.remove('circle')
+
+  updatePlayerDisplay();
 }
 
+// updates classes and turn display
+function updatePlayerDisplay() {
+  const p1Icon = document.getElementById('p1-turn-cross')
+  const p2Icon = document.getElementById('p2-turn-dot')
+
+  if (circleTurn) {
+    p1Icon.classList.remove('cross')
+    p2Icon.classList.add('circle')
+  } else {
+    p1Icon.classList.add('cross')
+    p2Icon.classList.remove('circle')
+  }
+  }
+
+// displays end screen or switches turn
 function handleClick(e) {
   const cell = e.target
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  const currentClass = circleTurn ? CIRCLE : CROSS
   placeMark(cell, currentClass)
   if (checkWin(currentClass)) {
     endGame(false)
@@ -47,39 +71,55 @@ function handleClick(e) {
   }
 }
 
+// implements ending screen
 function endGame(draw) {
   if (draw) {
-    winningMessageTextElement.innerText = 'Draw!'
+    winMsgTxt.innerText = 'Draw!'
   } else {
-    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    winMsgTxt.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
   }
-  winningMessageElement.classList.add('show')
+  winMsg.classList.add('show')
 }
 
+// checks if all cells are filled
 function isDraw() {
   return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    return cell.classList.contains(CROSS) || cell.classList.contains(CIRCLE)
   })
 }
 
+// places cross or circle on board
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass)
 }
 
+// switches player turn display
 function swapTurns() {
+  const p1Icon = document.getElementById('p1-turn-cross');
+  const p2Icon = document.getElementById('p2-turn-dot');
+
+  if (!circleTurn) {
+    p1Icon.style.visibility = 'hidden'; 
+    p2Icon.style.visibility = 'visible'; 
+  } else {
+    p1Icon.style.visibility = 'visible'; 
+    p2Icon.style.visibility = 'hidden'; 
+  }
   circleTurn = !circleTurn
 }
 
+// implement transparent icons during hovering
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS)
-  board.classList.remove(CIRCLE_CLASS)
+  board.classList.remove(CROSS)
+  board.classList.remove(CIRCLE)
   if (circleTurn) {
-    board.classList.add(CIRCLE_CLASS)
+    board.classList.add(CIRCLE)
   } else {
-    board.classList.add(X_CLASS)
+    board.classList.add(CROSS)
   }
 }
 
+// loops through all winning combinations to find possible match
 function checkWin(currentClass) {
   return WINNING_COMBINATIONS.some(combination => {
     return combination.every(index => {
